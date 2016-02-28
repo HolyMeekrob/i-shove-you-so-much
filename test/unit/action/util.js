bae483ba41d704b44ad919b48bb2e0574372800d
Andy Steinberg <holy.meekrob@gmail.com>
2016-02-28 07:50:36 -0600

    Implement tests for isTokenForCurrentPlayer

--- test/unit/action/util.js
+++ test/unit/action/util.js
@@ -1,7 +1,7 @@
 import test from 'tape';
 
 import {
-	getBorderAt, getFloorAt, isTokenForPlayer
+	getBorderAt, getFloorAt, isTokenForPlayer, isTokenForCurrentPlayer
 } from '../../../src/action/util';
 
 test('util.getBorderAt()', (assert) => {
@@ -54,7 +54,20 @@ test('util.getFloorAt()', (assert) => {
 	assert.end();
 });
 
-test('util.isTokenForPlayer()', (assert) => {
+test('util.isTokenForPlayer() given a token for the given player', (assert) => {
+	const playerOne = 1;
+
+	const token = {
+		getPlayerType: () => playerOne
+	}
+
+	assert.equal(isTokenForPlayer(playerOne, token), true,
+		'returns true');
+
+	assert.end();
+});
+
+test('util.isTokenForPlayer() given a token for another player', (assert) => {
 	const playerOne = 1;
 	const playerTwo = 2;
 
@@ -62,9 +75,38 @@ test('util.isTokenForPlayer()', (assert) => {
 		getPlayerType: () => playerOne
 	}
 
-	assert.equal(isTokenForPlayer(playerOne, token), true,
-		'returns true if the token is for the given player');
-	assert.equal(isTokenForPlayer(playerTwo, token), false,
-		'returns false if the token is not for the given player');
+	assert.equal(isTokenForPlayer(playerTwo, token), false, 'returns false');
+	assert.end();
+});
+
+test('util.isTokenForCurrentPlayer() given one of the current player\'s token',
+	(assert) => {
+	const playerTwo = 2;
+
+	const game = {
+		getTurn: () => playerTwo
+	};
+
+	const token = {
+		getPlayerType: () => playerTwo
+	};
+
+	assert.equal(isTokenForCurrentPlayer(game, token), true, 'returns true');
+	assert.end();
+});
+test('util.isTokenForCurrentPlayer() given a token for a different player than the current player',
+	(assert) => {
+	const playerOne = 1;
+	const playerTwo = 2;
+
+	const game = {
+		getTurn: () => playerOne
+	};
+
+	const token = {
+		getPlayerType: () => playerTwo
+	};
+
+	assert.equal(isTokenForCurrentPlayer(game, token), false, 'returns false');
 	assert.end();
 });
