ba5da1c1cea4598bd4cb8e79541d04602eda8f5b
Andy Steinberg <holy.meekrob@gmail.com>
2016-03-09 07:59:59 -0600

    Implement missing move validation functions

--- src/action/move/validation.ts
+++ src/action/move/validation.ts
@@ -1,6 +1,6 @@
 import { and, complement, curry, isNil, lift } from 'ramda';
 import {
-	getBorderAt, getFloorAt, getNextPosition
+	getBorderAt, getFloorAt, getNextPosition, isTokenForPlayer
 } from '../util';
 
 import { Border } from '../../model/border';
@@ -8,6 +8,7 @@ import { Direction } from '../../model/direction';
 import { Floor } from '../../model/floor';
 import { Game } from '../../model/game';
 import { Position } from '../../model/position';
+import { Token } from '../../model/token';
 
 const hasEmptyPath =
 (game: Game, pos: Position, dir: Direction, spacesRemaining: number): boolean => {
@@ -22,3 +23,22 @@ const hasEmptyPath =
 	return getBorderAt(game, pos, dir) !== Border.Wall
 		&& hasEmptyPath(game, getNextPosition(dir, pos), dir, spacesRemaining - 1);
 };
+
+
+const isMoveableToken = (game: Game, token: Token): boolean =>
+	lift(and)(complement(isNil), isTokenForPlayer(game.getTurn()))(token);
+
+
+export const validateMove =
+curry((game: Game, pos: Position, dir: Direction, spaces: number): boolean => {
+	if (spaces < 1) {
+		return false;
+	}
+
+	if (!game.getGameBoard().hasTokenAt(pos)) {
+		return false;
+	}
+
+	return isMoveableToken(game, game.getGameBoard().getTokenAt(pos))
+		&& hasEmptyPath(game, pos, dir, spaces - 1);
+});
